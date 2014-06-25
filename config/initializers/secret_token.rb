71f2e11205a4506c45b7f9758a93e5c6dcf0a954
hsichang <hsi.chang@gmail.com>
2014-06-25 11:23:22 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hsichanglin::Application.config.secret_token = '35e4cf542091529f3ac3f321acc74d424750d58ac9a9dc48e3fabeefd10fe2251412121ffe1fc9687588fc460ef468b0068bbeac2cda0dbad3e2b323c348e2bf'
